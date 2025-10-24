b409db9241dbca9bff5dd86dcbbac1b19bc2e94c
Kristianto <itukristiantoini@outlook.com>
2025-10-24 14:39:06 +0700

    (theming) - Refactor code structure for improved readability and maintainability

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+const Home = () => {
+	return (
+		<div className='bg-blue-500 text-white p-4 text-center'>
+			Tailwind Works 🎉
+		</div>
+	);
+};
+
+export default Home;
+
